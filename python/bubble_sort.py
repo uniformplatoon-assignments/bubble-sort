c0c80d9138f8c73eba2d95ebcfbf0e6ed0f60f21
jayaruhbee <ottoman-fruity.0m@icloud.com>
2023-06-22 17:20:10 -0400

    first solution

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,19 @@
 sequence = [4, 3, 5, 0, 1]
 swaps = 0
+operations = 0
 
-# Your Code Here
+n = len(sequence)
+for i in range(n - 1):
+    for j in range(0,n - i - 1):
+        operations += 1
+        # print(f"operations {operations}")
+        if  sequence[j] > sequence[j + 1]:
+            swaps += 1
+            sequence[j],sequence[j + 1] = sequence[j + 1],sequence[j]
+            # print(f"sequence[j]: {sequence[j]} , sequence[j + 1]  {sequence[j + 1]}, sequence {sequence}")
+            
+        
 
-print(f"Final result: {result}")
+print(f"Final result: {sequence}")
 print(f"Swaps: {swaps}")
+
